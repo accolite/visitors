0f6d543af1e4c997c3e0bedb7b94336dd18e21be
Yashavanta Byagawadi <yashavanta.byagawadi@accoliteindia.com>
2019-06-05 13:30:16 +0530

    Create visitor page - first commit.

--- visitor-frontend/app/visitor/visitor.js
+++ visitor-frontend/app/visitor/visitor.js
@@ -1,14 +1,32 @@
 'use strict';
 
-angular.module('myApp.visitor', ['ngRoute'])
+var app = angular.module('myApp.visitor', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
+app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider.when('/visitor', {
     templateUrl: 'visitor/visitor.html',
     controller: 'VisitorCtrl'
   });
-}])
+}]);
 
-.controller('VisitorCtrl', [function() {
-
+app.controller('VisitorCtrl', ['$scope', function ($scope) {
+  $scope.visitor = {
+    firstName: '',
+    lastName: '',
+    emailId: '',
+    phoneNumber: '',
+    idType: '',
+    idNumber: '',
+    inTime: '',
+    outTime: '',
+    contactPerson: '',
+    purpose: '',
+    officeLocation: '',
+    comingFrom: ''
+  };
+  $scope.createVisitor = function () {
+    console.log('pojo', $scope.visitor);
+    var inTimeDate = new Date($scope.visitor.inTime);
+    console.log('in time', inTimeDate);
+  };
 }]);
