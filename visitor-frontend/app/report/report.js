07d6e2845f0e71305a56b226ed81b897df4b6363
ashutoshkrgupta <ashutosh.gupta@accoliteindia.com>
2019-06-06 15:36:25 +0530

    Adding changes for report page

--- visitor-frontend/app/report/report.js
+++ visitor-frontend/app/report/report.js
@@ -9,6 +9,87 @@ angular.module('myApp.report', ['ngRoute'])
   });
 }])
 
-.controller('ReportCtrl', [function() {
+.controller('ReportCtrl', ['$scope', '$http', function($scope, $http) {
+
+
+
+// table headers that we need to show
+$scope.tableHeaders = {
+                       "firstName"    : 'First Name',     "lastName"      : 'Last Name',
+                       "phoneNumber"  : 'Phone Number',   "emailId"       : 'Email Id',
+                       "comingFrom"   : 'Coming From',    "officeLocation": 'Office Location',
+                       "idType"       : 'Gov Id Type',    "idNumber"      :  'Gov Id Number',
+                       "inTime"       : 'In Time',        "outTime"       : 'Out Time', 
+                       "contactPerson": 'Contact Person', "purpose"       : 'Purpose',
+                       "visitorType"  : 'Visitor Type',   "employeeId"    : 'Emp Id' };
+// if error in our request to the server
+function errorHandler(response) {s
+    if (!angular.isObject(response))
+    return null;
+    // if the response is an object return his value
+    return response.data;
+ 
+ };
+ 
+ // if success in our request to the server
+function successHandler(response) {
+  return response.data;
+};
+// get Clients Info from DataBase
+ function getClients() {
+    var response = $http.get("http://localhost:8081/api/visitor/", {},{});
+    return response.then(successHandler, errorHandler);
+ };
+// fill our table
+function headFilter(val){
+  // include header what you want to exclude
+ var excludeHead =['id'];
+ return !excludeHead.includes(val);
+
+}
+function fillTable() {
+   getClients()
+   .then(function(response){
+       $scope.lstClients = response;
+       $scope.headElement= (Object.keys($scope.lstClients[0])).filter(headFilter);
+       console.log(Object.keys($scope.lstClients[0]));
+   })
+   .catch(function(response) {
+       console.log(response); 
+ });
+};
+
+
+
+$scope.setExitTime= function(id){
+  console.log($scope.lstClients);
+
+  $http.put('http://localhost:8081/api/visitor/exit/' + id).then(function (response) {
+
+    if (response.data)
+    
+    $scope.msg = "Put Data Method Executed Successfully!";
+    
+    }, function (response) {
+    
+    $scope.msg = "Service not Exists";
+    
+    $scope.statusval = response.status;
+    
+    $scope.statustext = response.statusText;
+    
+    $scope.headers = response.headers();
+    
+    });
+
+}
+
+$scope.editable = true;
+$scope.editItem = function(id){
+  $scope.editable = !$scope.editable;
+}
+fillTable();
+
 
 }]);
+
