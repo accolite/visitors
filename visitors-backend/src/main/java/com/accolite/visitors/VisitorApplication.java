099a410a7e2c9e28b67d07ec3a52ce2669a15472
Lavanya Srinivasan <lavanya.srinivasan@accoliteindia.com>
2019-06-12 16:57:33 +0530

    added SpringBootServletInitializer for API issue in server

--- visitors-backend/src/main/java/com/accolite/visitors/VisitorApplication.java
+++ visitors-backend/src/main/java/com/accolite/visitors/VisitorApplication.java
@@ -5,15 +5,25 @@ package com.accolite.visitors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 /**
  * @author Lavanya
  *
  */
 @SpringBootApplication
-public class VisitorApplication {
+@EnableMongoRepositories(basePackages = "com.accolite.visitors.dao")
+public class VisitorApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VisitorApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(VisitorApplication.class);
+	}
+
 }
