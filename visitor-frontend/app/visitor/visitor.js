30901f704cbcd0c3d9cc98cb24e828aa88d18cd8
Savin <savinpaul.sathyan@accoliteindia.com>
2019-06-20 12:29:07 +0530

    Validation changes

--- visitor-frontend/app/visitor/visitor.js
+++ visitor-frontend/app/visitor/visitor.js
@@ -12,48 +12,26 @@ app.config(['$routeProvider', function ($routeProvider) {
 app.controller('VisitorCtrl', ['$scope', '$http','$routeParams', function ($scope, $http,$routeParams ) {
   var urlPart = window.location.href.split("?")[1];
   if(urlPart != null){
- var loc = (urlPart).substr(urlPart.indexOf("loc=") + 4);
+    var loc = (urlPart).substr(urlPart.indexOf("loc=") + 4);
   }
 
-
 var url = "http://visitors.accolitelabs.com/visitors/api/visitor";
-//var url = "http://localhost:8081/api/visitor"
- console.log("aaaa" + loc);
- var locations ={ 
-         "BLR" : "Bangalore",
-         "HYD" : "Hyderabad",
-         "DEL" : "Delhi",
-         "" : "",
- }
- console.log(locations[loc]);
-  var currentDat= new Date();
-  $scope.isSuccess = false;
-  $scope.isFailure = false;
 
-  console.log("afaaff" + $routeParams.parm);
-  $scope.visitor = {
-    firstName: 'Yashavanta',
-    lastName: 'Byagawadi',
-    emailId: 'yashavanta92@gmail.com',
-    phoneNumber: 8105236319,
-    idType: 'Voter Id',
-    idNumber: '12345',
-    inTime: currentDat,
-    outTime: '',
-    contactPerson: 'Srikanth  Jenkins',
-    purpose: 'Interview',
-    officeLocation: locations[loc],
-    comingFrom: 'BTM',
-    visitorType: 'Guest',
-    employeeId: '1111'
-  };
+var locations ={ 
+        "BLR" : "Bangalore",
+        "HYD" : "Hyderabad",
+        "DEL" : "Delhi",
+        "" : "",
+}
+
+ var currentDat= new Date();
+ $scope.isSuccess = false;
+ $scope.isFailure = false;
+  
   $scope.createVisitor = function () {
     $scope.isSuccess = false;
-    $scope.isFailure = false;
-   
-    console.log('pojo', $scope.visitor);
+    $scope.isFailure = false
     var inTimeDate = new Date($scope.visitor.inTime);
-    
 
     $http.post(url + "/create", $scope.visitor)
       .then(function myResponse(response) {
@@ -64,8 +42,7 @@ var url = "http://visitors.accolitelabs.com/visitors/api/visitor";
         }else{
           $scope.isFailure = true;
           $scope.failureMsg = response.statusText;
-        }
-
+        }                
       }, function myError(response) {
         console.log('error response ', response);
         $scope.isFailure = true;
