70fc7d3b8db5531805ea22dbc74ccc47c662a026
shashikant <shashikant.mittapelli@accoliteindia.com>
2019-07-13 17:37:40 +0530

    Shashikant-7 Created card component

--- visitors-frontend/src/app/modules/material.module.ts
+++ visitors-frontend/src/app/modules/material.module.ts
@@ -6,16 +6,23 @@
  */
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatCheckboxModule, MatTableModule, MatFormFieldModule, MatInputModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import {
+  MatButtonModule, MatCheckboxModule, MatTableModule, MatFormFieldModule,
+  MatInputModule, MatPaginatorModule, MatSortModule, MatCardModule, MatIconModule
+} from '@angular/material';
 import { DataTableComponent } from '../components/ui-components/data-table/data-table.component';
+import { CardComponent } from '../components/ui-components/card/card.component';
+
+const componentDeclarations = [ DataTableComponent, CardComponent ]
 
 @NgModule( {
-  declarations: [ DataTableComponent ],
+  declarations: [ ...componentDeclarations ],
   imports: [ CommonModule, MatButtonModule, MatCheckboxModule, MatTableModule,
-    MatFormFieldModule, MatInputModule, MatPaginatorModule, MatSortModule ],
-  exports: [ CommonModule, MatButtonModule, MatCheckboxModule, MatTableModule,
     MatFormFieldModule, MatInputModule, MatPaginatorModule, MatSortModule,
-    DataTableComponent ]
+    MatIconModule, MatCardModule ],
+  exports: [ CommonModule, MatButtonModule, MatCheckboxModule, MatTableModule,
+    MatFormFieldModule, MatInputModule, MatPaginatorModule, MatSortModule, MatCardModule,
+    MatIconModule, ...componentDeclarations ]
 } )
 
 export class MaterialModule { }
