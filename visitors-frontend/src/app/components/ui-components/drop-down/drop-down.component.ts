4da36a92af355b3dd71f08415dcb7258b1afc1ac
shashikant <shashikant.mittapelli@accoliteindia.com>
2019-07-15 15:13:17 +0530

    Added Drop down component and refactored code

---
+++ visitors-frontend/src/app/components/ui-components/drop-down/drop-down.component.ts
@@ -0,0 +1,36 @@
+/**
+ * @author M.Shashikant(shashikant.mittapelli@accoliteindia.com)
+ * 
+ * Gives Flexibility to create Material Dropdown component
+ */
+
+import { Component, Input, Output, EventEmitter, ChangeDetectorRef } from '@angular/core';
+
+@Component( {
+  selector: 'drop-down',
+  templateUrl: 'drop-down.component.html',
+  styleUrls: [ 'drop-down.component.css' ]
+} )
+
+
+export class DropdownComponent {
+  @Input()
+  data: Array<any>;
+
+  @Input()
+  id: string;
+
+  @Input()
+  label: string;
+
+  @Input()
+  selectedValue: string;
+
+  @Output()
+  selectedValueChange: EventEmitter<any> = new EventEmitter<any>();
+
+  emitChanges() {
+    this.selectedValueChange.emit( this.selectedValue );
+  }
+
+}
