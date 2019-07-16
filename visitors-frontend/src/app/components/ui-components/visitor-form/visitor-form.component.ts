e12bdb0eb36397505337596d6e57534e492af621
IamShashikant <shashikant.mittapelli@accoliteindia.com>
2019-07-16 03:16:37 -0700

    completed form work

--- visitors-frontend/src/app/components/ui-components/visitor-form/visitor-form.component.ts
+++ visitors-frontend/src/app/components/ui-components/visitor-form/visitor-form.component.ts
@@ -1,71 +1,65 @@
+import { VisitorService } from "src/app/services/visitor.service";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
+import { Component } from "@angular/core";
 
-import { VisitorService } from 'src/app/services/visitor.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
-
-
-@Component( {
-
-  selector: 'app-visitor-form',
-  templateUrl: './visitor-form.component.html',
-  styleUrls: ['./visitor-form.component.css']
-} )
+@Component({
+  selector: "app-visitor-form",
+  templateUrl: "./visitor-form.component.html",
+  styleUrls: ["./visitor-form.component.css"]
+})
 export class VisitorFormComponent {
+  constructor(public service: VisitorService, private http: HttpClient) {}
+  visitorType = ["Guest", "Employee"];
 
-  constructor(public service: VisitorService, private http: HttpClient) { }
-  visitorType = ['Guest','Employee'];
-
-  ids = ['Voter Id', 'PAN', 'Adhaar'];
+  ids = ["Voter Id", "PAN", "Adhaar"];
   now;
   purposes = [
-    { id: 1, value: 'Interviewing' },
-    { id: 2, value: 'Meeting' },
-    { id: 3, value: 'New Joinee' }
+    { id: 1, value: "Interviewing" },
+    { id: 2, value: "Meeting" },
+    { id: 3, value: "New Joinee" }
   ];
-  mobnumPattern = '^((\\+91-?)|0)?[0-9]{10}$';
+  mobnumPattern = "^((\\+91-?)|0)?[0-9]{10}$";
 
   form: FormGroup = new FormGroup({
-    firstName: new FormControl('', Validators.required),
-    lastName: new FormControl('', Validators.required),
-    employeeId: new FormControl('', Validators.required),
+    firstName: new FormControl("", Validators.required),
+    lastName: new FormControl("", Validators.required),
+    employeeId: new FormControl("", Validators.required),
     inTime: new FormControl(),
-    contactPerson: new FormControl(''),
-    emailId: new FormControl('', Validators.email),
-    phoneNumber: new FormControl('', [
+    contactPerson: new FormControl(""),
+    emailId: new FormControl("", Validators.email),
+    phoneNumber: new FormControl("", [
       Validators.required,
       Validators.pattern(this.mobnumPattern)
     ]),
-    comingFrom: new FormControl(''),
-    idType: new FormControl(''),
-    purpose: new FormControl(''),
-    idNumber: new FormControl(''),
-    officeLocation: new FormControl(''),
-    visitorType: new FormControl('')
+    comingFrom: new FormControl(""),
+    idType: new FormControl(""),
+    purpose: new FormControl(""),
+    idNumber: new FormControl(""),
+    officeLocation: new FormControl(""),
+    visitorType: new FormControl("")
   });
 
   initializeFormGroup() {
     this.form.setValue({
-      firstName: '',
-      lastName: '',
-      emailId: '',
-      idType: '',
-      contactPerson: '',
-      phoneNumber: '',
-      comingFrom: '',
-      purpose: '',
-      inTime: '',
-      idNumber: '',
-      officeLocation: '',
-      visitorType: ''
+      firstName: "",
+      lastName: "",
+      emailId: "",
+      idType: "",
+      contactPerson: "",
+      phoneNumber: "",
+      comingFrom: "",
+      purpose: "",
+      inTime: "",
+      idNumber: "",
+      officeLocation: "",
+      visitorType: ""
     });
   }
   ngDoCheck(): void {
-
-    setTimeout(() => { this.now = new Date();
-    }, 2000)
-
-
+    setTimeout(() => {
+      this.now = new Date();
+    }, 2000);
   }
 
   onClear() {
@@ -73,30 +67,18 @@ export class VisitorFormComponent {
     this.initializeFormGroup();
   }
   onSubmit() {
-     this.service.createNewVisitor(this.form.value).subscribe(
-       (val) => {
-             console.log(val);
-       }
-     );
-     this.form.reset();
-     this.initializeFormGroup();
-
-
-  }
-  onSubmitEmployeeData(){
-    this.service.createNewVisitor(this.form.value).subscribe(
-      (val) => {
-            console.log(val);
-      }
-    );
+    this.service.createNewVisitor(this.form.value).subscribe(val => {
+      console.log(val);
+    });
     this.form.reset();
-
-
+    this.initializeFormGroup();
+  }
+  onSubmitEmployeeData() {
+    this.service.createNewVisitor(this.form.value).subscribe(val => {
+      console.log(val);
+    });
+    this.form.reset();
   }
 
-  ngOnInit() {
-
-
-  }
-
+  ngOnInit() {}
 }
--- visitors-frontend/src/app/modules/visitors.module.ts
+++ visitors-frontend/src/app/modules/visitors.module.ts
@@ -3,7 +3,7 @@ import { VisitorFormComponent } from "../components/ui-components/visitor-form/v
 import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MaterialModule } from "./material.module";
-import { RouterModule } from '@angular/router';
+import { RouterModule } from "@angular/router";
 
 const routes = [
   {
@@ -14,7 +14,13 @@ const routes = [
 
 @NgModule({
   declarations: [VisitorFormComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule, RouterModule.forChild(routes)],
-  exports: [VisitorFormComponent ,RouterModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [VisitorFormComponent, RouterModule]
 })
 export class VisitorsModule {}
