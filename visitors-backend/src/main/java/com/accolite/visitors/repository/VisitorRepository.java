5686ea53906d3d2945754762939fffc7972f672b
Lavanya Srinivasan <lavanya.srinivasan@accoliteindia.com>
2019-07-18 17:42:45 +0530

    added updateVisitSummary API, FAILED status, VisitSummaryNotFoundException

--- visitors-backend/src/main/java/com/accolite/visitors/repository/VisitorRepository.java
+++ visitors-backend/src/main/java/com/accolite/visitors/repository/VisitorRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
+import com.accolite.visitors.model.VisitSummary;
 import com.accolite.visitors.model.Visitor;
 
 /**
@@ -15,19 +16,19 @@ import com.accolite.visitors.model.Visitor;
  *
  */
 @Repository
-public interface VisitorRepository extends MongoRepository<Visitor, String>, VisitorDAL, QuerydslPredicateExecutor<Visitor>{
+public interface VisitorRepository extends MongoRepository<Visitor, String>, VisitorDAL, QuerydslPredicateExecutor<Visitor> {
 
-	//@Query(fields = "{'visitSummary':0}")
-	//public Optional<Visitor> findByEmailId(String email);
-	
+	// @Query(fields = "{'visitSummary':0}")
+	// public Optional<Visitor> findByEmailId(String email);
+
 	@Query(fields = "{'visitSummary.$':1}")
 	public List<Visitor> findByVisitSummary_InTimeBetweenOrderByVisitSummary_InTimeDesc(Date startDate, Date endDate);
-	
-	//@Query()
-	//public List<Visitor> findAllByOrderByInTimeDesc();
-	
-//	public List<VisitSummary> findByVisitorIn(Set<String> ids);
-	
+
+	// @Query()
+	// public List<Visitor> findAllByOrderByInTimeDesc();
+
+	// public List<VisitSummary> findByVisitorIn(Set<String> ids);
+
 	public void deleteById(String id);
-	
+
 }
