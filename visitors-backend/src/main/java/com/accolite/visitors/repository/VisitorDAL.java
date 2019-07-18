afc81eb587cbed964a245fcc9d1dba2edba9848f
Shadyaab-Accolite <shadyaab.akhtar@accoliteindia.com>
2019-07-18 12:59:25 +0530

    updating status of visit

--- visitors-backend/src/main/java/com/accolite/visitors/repository/VisitorDAL.java
+++ visitors-backend/src/main/java/com/accolite/visitors/repository/VisitorDAL.java
@@ -1,11 +1,16 @@
 package com.accolite.visitors.repository;
 
+import java.util.Optional;
+
 import com.accolite.visitors.model.VisitSummary;
+import com.accolite.visitors.model.Visitor;
 
 public interface VisitorDAL {
 
 	long updateEndTime(String id);
 	
 	long addVisit(String id, VisitSummary visitor);
+	
+	public Optional<Visitor> findByEmailId(String email);
 
 }
--- visitors-backend/src/main/java/com/accolite/visitors/repository/VisitorDALImpl.java
+++ visitors-backend/src/main/java/com/accolite/visitors/repository/VisitorDALImpl.java
@@ -4,35 +4,64 @@ import static org.springframework.data.mongodb.core.query.Criteria.where;
 import static org.springframework.data.mongodb.core.query.Query.query;
 import static org.springframework.data.mongodb.core.query.Update.update;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.Optional;
 
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationOperation;
+import org.springframework.data.mongodb.core.aggregation.AggregationOperationContext;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.core.query.Update;
 
+import com.accolite.visitors.enums.VisitorStatus;
 import com.accolite.visitors.model.VisitSummary;
 import com.accolite.visitors.model.Visitor;
 
-public class VisitorDALImpl implements VisitorDAL{
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
+
+public class VisitorDALImpl implements VisitorDAL {
 
 	@Autowired
 	private MongoTemplate mongoTemplate;
-	
+
 	@Override
 	public long updateEndTime(String id) {
+		Update update = new Update();
+		update.set("visitSummary.$.outTime", new Date());
+		update.set("visitSummary.$.status", VisitorStatus.COMPLETED);
 		return mongoTemplate.updateFirst(query(where("id").is(id).andOperator(where("visitSummary.outTime").is(null))),
-				update("visitSummary.$.outTime", new Date()), Visitor.class).getModifiedCount();
-		
+				update, Visitor.class).getModifiedCount();
+
 	}
 
 	@Override
 	public long addVisit(String id, VisitSummary visitSummary) {
 		Update update = new Update();
 		update.addToSet("visitSummary", visitSummary);
-		
+
 		return mongoTemplate.updateFirst(query(where("id").is(id)), update, Visitor.class).getModifiedCount();
 	}
-	
-	
+
+	public Optional<Visitor> findByEmailId(String email) {
+
+		Aggregation aggregation = newAggregation(match(where("emailId").is(email)), new AggregationOperation() {
+
+			@Override
+			public Document toDocument(AggregationOperationContext context) {
+
+				Document document = new Document("$addFields", new Document("visitSummary",
+						Arrays.asList(new Document("$arrayElemAt", Arrays.asList("$visitSummary", -1)))));
+				return document;
+			}
+		});
+		
+		AggregationResults<Visitor> result = mongoTemplate.aggregate(aggregation, Visitor.class, Visitor.class);
+
+		return Optional.ofNullable(result.getUniqueMappedResult());
+	}
 
 }
