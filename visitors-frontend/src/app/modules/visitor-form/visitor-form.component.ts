52be538f45a2c0f6df0993151387a346b0dae535
seema-bhooteshwar <seema.bhooteshwar@accoliteindia.com>
2019-07-17 20:35:35 -0700

    bug fixed

--- visitors-frontend/src/app/modules/visitor-form/visitor-form.component.ts
+++ visitors-frontend/src/app/modules/visitor-form/visitor-form.component.ts
@@ -2,17 +2,19 @@ import { VisitorService } from "src/app/services/visitor.service";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
+import { timestamp } from "rxjs/operators";
+import { observable, Observable } from "rxjs";
 
-@Component( {
+@Component({
   selector: "app-visitor-form",
   templateUrl: "./visitor-form.component.html",
-  styleUrls: [ "./visitor-form.component.css" ]
-} )
+  styleUrls: ["./visitor-form.component.css"]
+})
 export class VisitorFormComponent {
-  constructor( public service: VisitorService, private http: HttpClient ) { }
-  visitorType = [ "Guest", "Employee" ];
+  constructor(public service: VisitorService, private http: HttpClient) {}
+  visitorType = ["Guest", "Employee"];
 
-  ids = [ "Voter Id", "PAN", "Adhaar" ];
+  ids = ["Voter Id", "PAN", "Adhaar"];
   now;
   purposes = [
     { id: 1, value: "Interviewing" },
@@ -21,27 +23,27 @@ export class VisitorFormComponent {
   ];
   mobnumPattern = "^((\\+91-?)|0)?[0-9]{10}$";
 
-  form: FormGroup = new FormGroup( {
-    firstName: new FormControl( "", Validators.required ),
-    lastName: new FormControl( "", Validators.required ),
-    employeeId: new FormControl( "", Validators.required ),
+  form: FormGroup = new FormGroup({
+    firstName: new FormControl("", Validators.required),
+    lastName: new FormControl("", Validators.required),
+    employeeId: new FormControl(new Date().getSeconds(), Validators.required),
     inTime: new FormControl(),
-    contactPerson: new FormControl( "" ),
-    emailId: new FormControl( "", Validators.email ),
-    phoneNumber: new FormControl( "", [
+    contactPerson: new FormControl(""),
+    emailId: new FormControl("", [Validators.email, Validators.required]),
+    phoneNumber: new FormControl("", [
       Validators.required,
-      Validators.pattern( this.mobnumPattern )
-    ] ),
-    comingFrom: new FormControl( "" ),
-    idType: new FormControl( "" ),
-    purpose: new FormControl( "" ),
-    idNumber: new FormControl( "" ),
-    officeLocation: new FormControl( "" ),
-    visitorType: new FormControl( "" )
-  } );
+      Validators.pattern(this.mobnumPattern)
+    ]),
+    comingFrom: new FormControl(""),
+    idType: new FormControl(""),
+    purpose: new FormControl(""),
+    idNumber: new FormControl(""),
+    officeLocation: new FormControl(""),
+    visitorType: new FormControl("", Validators.required)
+  });
 
   initializeFormGroup() {
-    this.form.setValue( {
+    this.form.setValue({
       firstName: "",
       lastName: "",
       emailId: "",
@@ -54,33 +56,41 @@ export class VisitorFormComponent {
       idNumber: "",
       officeLocation: "",
       visitorType: "",
-      employeeId: ""
-    } );
+      employeeId: " "
+    });
   }
 
   ngDoCheck(): void {
-    setTimeout( () => {
+    setTimeout(() => {
       this.now = new Date();
-    }, 2000 );
+    }, 2000);
   }
 
   onClear() {
     this.form.reset();
-    this.initializeFormGroup();
   }
   onSubmit() {
-    this.service.createNewVisitor( this.form.value ).subscribe( val => {
-      console.log( val );
-    } );
+    this.form.controls["employeeId"].setValue(new Date().getSeconds());
+    console.log("print" + this.form.controls["firstName"].invalid);
+    console.log("print" + this.form.controls["lastName"].invalid);
+    console.log("print" + this.form.controls["emailId"].invalid);
+    console.log("print" + this.form.controls["phoneNumber"].invalid);
+    console.log("print" + this.form.controls["contactPerson"].invalid);
+    console.log("print" + this.form.controls["comingFrom"].invalid);
+    console.log("print" + this.form.controls["idType"].invalid);
+    console.log("print" + this.form.controls["employeeId"].invalid);
+    this.service.createNewVisitor(this.form.value).subscribe(val => {
+      console.log(val);
+    });
     this.form.reset();
     this.initializeFormGroup();
   }
   onSubmitEmployeeData() {
-    this.service.createNewVisitor( this.form.value ).subscribe( val => {
-      console.log( val );
-    } );
+    this.service.createNewVisitor(this.form.value).subscribe(val => {
+      console.log(val);
+    });
     this.form.reset();
   }
 
-  ngOnInit() { }
+  ngOnInit() {}
 }
