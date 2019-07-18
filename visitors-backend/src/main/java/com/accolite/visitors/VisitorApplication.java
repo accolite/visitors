6cb3139d2a8dadc72c68e3d07733287ebd0c9b7f
Lavanya Srinivasan <lavanya.srinivasan@accoliteindia.com>
2019-07-18 11:29:58 +0530

    lambda test

--- visitors-backend/src/main/java/com/accolite/visitors/VisitorApplication.java
+++ visitors-backend/src/main/java/com/accolite/visitors/VisitorApplication.java
@@ -3,11 +3,21 @@ package com.accolite.visitors;
  * 
  */
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 /**
  * @author Lavanya
@@ -26,4 +36,37 @@ public class VisitorApplication extends SpringBootServletInitializer {
 		return application.sources(VisitorApplication.class);
 	}
 
+	/*
+     * Create required HandlerMapping, to avoid several default HandlerMapping instances being created
+     */
+    @Bean
+    public HandlerMapping handlerMapping() {
+        return new RequestMappingHandlerMapping();
+    }
+
+    /*
+     * Create required HandlerAdapter, to avoid several default HandlerAdapter instances being created
+     */
+    @Bean
+    public HandlerAdapter handlerAdapter() {
+        return new RequestMappingHandlerAdapter();
+    }
+
+    /*
+     * optimization - avoids creating default exception resolvers; not required as the serverless container handles
+     * all exceptions
+     *
+     * By default, an ExceptionHandlerExceptionResolver is created which creates many dependent object, including
+     * an expensive ObjectMapper instance.
+     */
+    @Bean
+    public HandlerExceptionResolver handlerExceptionResolver() {
+        return new HandlerExceptionResolver() {
+
+            @Override
+            public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+                return null;
+            }
+        };
+    }
 }
