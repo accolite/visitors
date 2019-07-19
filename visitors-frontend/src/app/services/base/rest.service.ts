d34aedc5ab4551a4b39d4ba201625878ddc451bb
shashikant <shashikant.mittapelli@accoliteindia.com>
2019-07-19 16:07:24 +0530

    Refactored snackbar code

--- visitors-frontend/src/app/services/base/rest.service.ts
+++ visitors-frontend/src/app/services/base/rest.service.ts
@@ -7,7 +7,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material';
 import { Observer } from 'rxjs';
-import { accoliteSnackbarMessages } from '../../helpers/static-data'
+import { accoliteSnackbarMessages } from '../../helpers/static-data';
+import { SnackbarComponent } from 'src/app/components/ui-components/snack-bar/snack-bar.component';
 
 @Injectable( {
   providedIn: 'root'
@@ -16,23 +17,6 @@ import { accoliteSnackbarMessages } from '../../helpers/static-data'
 export class RestService {
 
   constructor( private http: HttpClient, private snackbarService: MatSnackBar ) { }
-
-  createSnackbar( message: string, action: string = "close", durationMS: number = 2000 ) {
-    this.snackbarService.open( message, action, { duration: durationMS } )
-  }
-
-  createNotifySnackbar( key: string, action: string = "close", durationMS: number = 2000 ) {
-    let message = ( key ) => ( accoliteSnackbarMessages[ 'rest' ] )[ key ];
-    let observer: Observer<any> = {
-      next: ( data ) => {
-        this.createSnackbar( message( 'success' )[ key ], action, durationMS )
-      }, error: ( error ) => {
-        this.createSnackbar( message( 'error' )[ key ], action, durationMS )
-      }, complete: () => {
-      }
-    }
-    return observer
-  }
 
   get( url: string, options: { params?: HttpParams, headers?: HttpHeaders } = {} ) {
     return this.http.get( url, options )
@@ -48,6 +32,33 @@ export class RestService {
 
   delete( url: string, options: { params?: HttpParams, headers?: HttpHeaders } = {} ) {
     return this.http.delete( url, options )
+  }
+
+  createSnackbar( message: string, action: string = "close", durationMS: number = 2000, cssClass: string, icon?: string ) {
+    this.snackbarService.openFromComponent( SnackbarComponent, {
+      data: {
+        message: message,
+        icon: icon ? icon : 'info',
+        action: action,
+        class: cssClass
+      },
+      duration: durationMS,
+      verticalPosition: 'top',
+      horizontalPosition: 'right'
+    } )
+  }
+
+  createNotifySnackbar( key: string, action: string = "close", durationMS: number = 2000 ) {
+    let message = ( key ) => ( accoliteSnackbarMessages[ 'rest' ] )[ key ];
+    let observer: Observer<any> = {
+      next: ( data ) => {
+        this.createSnackbar( message( 'success' )[ key ], action, durationMS, "success" )
+      }, error: ( error ) => {
+        this.createSnackbar( message( 'error' )[ key ], action, durationMS, "error" )
+      }, complete: () => {
+      }
+    }
+    return observer
   }
 
 }
