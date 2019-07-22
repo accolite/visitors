acefa3f3ba38b1dcb2e25b45820e751070c25371
Shadyaab-Accolite <shadyaab.akhtar@accoliteindia.com>
2019-07-22 22:17:02 +0530

    Gridfs Image retrieval

--- visitors-backend/src/main/java/com/accolite/visitors/service/UploadServiceImpl.java
+++ visitors-backend/src/main/java/com/accolite/visitors/service/UploadServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,9 +22,11 @@ public class UploadServiceImpl implements UploadService{
 	
 	@Override
 	public String uploadFile(MultipartFile file, String title, String id) throws IOException, IllegalAccessException {
-		String imageId = repository.uploadFile(file, title);
+		String imageId = repository.uploadFile(file, title, id);
 		
 		//Add Image Id to the visitor Details
+		//Can be removed if not required since we are having reference of 
+		//visitor in the image collection itself
 		Map<String, Object> visitorMap = new HashMap<>();
 		visitorMap.put("imageId", imageId);
 		visitorService.updateVisitorDetails(id, visitorMap);
@@ -31,4 +34,9 @@ public class UploadServiceImpl implements UploadService{
 		return imageId;
 	}
 
+	@Override
+	public GridFsResource getFile(String id) {
+		return repository.getFile(id);
+	}
+	
 }
