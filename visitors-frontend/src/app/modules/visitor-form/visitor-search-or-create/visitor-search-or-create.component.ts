4c1290de0434d7dbc20485d9bb982bb829bd0e9c
shashikant <shashikant.mittapelli@accoliteindia.com>
2019-07-24 21:39:07 +0530

    third commit

--- visitors-frontend/src/app/modules/visitor-form/visitor-search-or-create/visitor-search-or-create.component.ts
+++ visitors-frontend/src/app/modules/visitor-form/visitor-search-or-create/visitor-search-or-create.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Subject } from 'rxjs';
 import { debounceTime, switchMap, distinctUntilChanged, filter } from 'rxjs/operators';
 import { idType } from 'src/app/helpers/static-data';
 import { VisitorModel } from 'src/app/helpers/models/visitors/visitors.model';
 import { VisitorService } from 'src/app/services/visitor.service';
 import { VisitSummaryModel } from 'src/app/helpers/models/visitors/visit-summary.model';
+import { NgForm, FormControl } from '@angular/forms';
+import { MatStep } from '@angular/material';
+import { Router } from '@angular/router';
 @Component( {
   selector: 'visitor-search-or-create',
   templateUrl: 'visitor-search-or-create.component.html',
@@ -13,14 +16,20 @@ import { VisitSummaryModel } from 'src/app/helpers/models/visitors/visit-summary
 export class VisitorSearchOrCreateComponent {
   search$: Subject<string> = new Subject<string>();
   user: VisitorModel;
+  data: any;
+  search: string;
   model: { [ propName: string ]: any } = {}
   idTypes: Array<string> = idType;
-  lastKeyPress: number = 0
+  lastKeyPress: number = 0;
+  @ViewChild( 'heroForm', { static: true } )
+  heroForm: NgForm;
 
-  constructor( private visitorService: VisitorService ) {
-    this.user = new VisitorModel();
-    this.user.visitSummary = [];
-    this.user.visitSummary.push( new VisitSummaryModel() );
+  @ViewChild( 'step', { static: true } )
+  step: MatStep
+
+
+  constructor( private visitorService: VisitorService, private router: Router ) {
+    this.createUser();
   }
 
   ngOnInit() {
@@ -30,18 +39,35 @@ export class VisitorSearchOrCreateComponent {
       filter( ( data ) => data != '' ),
       switchMap( ( data ) => this.visitorService.getVisitorByEmailId( data ) )
     ).subscribe( ( data ) => {
-      this.user = new VisitorModel( data );
+      this.data = data;
+      if ( data ) {
+        this.user = new VisitorModel( data );
+      } else {
+        this.createUser();
+        this.user.emailId = this.search
+      }
     } )
   }
 
+  createUser() {
+    this.user = new VisitorModel();
+    this.user.visitSummary = [];
+    this.user.visitSummary.push( new VisitSummaryModel() );
+  }
+
   searchValues( value: string, type: string ) {
+    this.search = value;
     let regex = type === 'phoneNumber' ? `[0-9]+` : `[^@]+@[^\.]+\..+`;
     let data = value.match( regex )
     if ( type === 'email' && data ) {
       this.search$.next( value );
-    } else if ( type === 'phoneNumber' && data ) {
-      this.search$.next( value );
     }
   }
 
+  onSubmit() {
+    this.visitorService.createNewVisitor( this.user ).subscribe( () => {
+      this.createUser();
+      this.router.navigateByUrl( '/visitor' );
+    } )
+  }
 }
