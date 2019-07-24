51b063c46c92b2f918dbbf9fc83150fb2f668a87
Lavanya Srinivasan <51190910+lavanya1204@users.noreply.github.com>
2019-07-24 20:56:25 +0530

    adding @EnableWebMvc in SwaggerConfig

--- visitors-backend/src/main/java/com/accolite/visitors/configuration/SwaggerConfig.java
+++ visitors-backend/src/main/java/com/accolite/visitors/configuration/SwaggerConfig.java
@@ -7,6 +7,7 @@ import static springfox.documentation.builders.PathSelectors.regex;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -21,6 +22,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
+@EnableWebMvc
 public class SwaggerConfig {
 
 	@Bean
