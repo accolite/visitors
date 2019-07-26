2b53321dbae4f1640a832f9889d14f41d76fd32f
seema-bhooteshwar <seema.bhooteshwar@accoliteindia.com>
2019-07-26 03:57:03 -0700

    bug fixed

--- visitors-frontend/src/app/modules/home/approved-request/approved-request.component.ts
+++ visitors-frontend/src/app/modules/home/approved-request/approved-request.component.ts
@@ -5,14 +5,17 @@ import { VisitorService } from "src/app/services/visitor.service";
 import { ServiceSearchParamsInputModel } from "src/app/helpers/models/service-search-params-input.model";
 import { PendingRequestComponent } from "../pending-request/pending-request.component";
 import { PreApprovedRequestComponent } from "../pre-approved-request/pre-approved-request.component";
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from "@angular/router";
+import { tap } from "rxjs/operators";
+import { RestService } from "src/app/services/base/rest.service";
 
-@Component( {
+@Component({
   selector: "app-approved-request",
   templateUrl: "./approved-request.component.html",
-  styleUrls: [ "./approved-request.component.css" ]
-} )
-export class ApprovedRequestComponent extends DataObtainer<any> implements OnInit {
+  styleUrls: ["./approved-request.component.css"]
+})
+export class ApprovedRequestComponent extends DataObtainer<any>
+  implements OnInit {
   visitors: any;
   pagination = false;
   searchObj = {};
@@ -24,32 +27,37 @@ export class ApprovedRequestComponent extends DataObtainer<any> implements OnIni
   @Input()
   preApproved: PreApprovedRequestComponent;
 
-  @ViewChild( MatPaginator, { static: true } )
+  @ViewChild(MatPaginator, { static: true })
   paginator: MatPaginator;
 
-  @ViewChild( MatSort, { static: true } )
+  @ViewChild(MatSort, { static: true })
   sort: MatSort;
 
   @Input()
   dataSource: MatTableDataSource<any>;
 
-  displayedColumns = [ "Name", "badgeNo", "inTime", "actions", "remarks" ];
+  displayedColumns = ["Name", "badgeNo", "inTime", "actions", "remarks"];
   @Input()
   ofcLocation: any;
 
-  constructor( private visitorService: VisitorService, private zone: NgZone, private route: ActivatedRoute ) {
-    super( zone );
+  constructor(
+    private visitorService: VisitorService,
+    private zone: NgZone,
+    private route: ActivatedRoute,
+    private rest: RestService
+  ) {
+    super(zone);
   }
 
-  getDataObservable( params: ServiceSearchParamsInputModel ) {
+  getDataObservable(params: ServiceSearchParamsInputModel) {
     this.searchObj = {
-      status: 'APPROVED',
+      status: "APPROVED",
       officeLocation: this.ofcLocation
     };
-    return this.visitorService.searchVisitor( this.searchObj );
+    return this.visitorService.searchVisitor(this.searchObj);
   }
 
-  onAfterUpdateData( data: any ) {
+  onAfterUpdateData(data: any) {
     this.visitors = data && data.data ? data.data : null;
     this.dataSource = new MatTableDataSource(
       this.visitors ? this.visitors : []
@@ -58,19 +66,20 @@ export class ApprovedRequestComponent extends DataObtainer<any> implements OnIni
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
-  exit( data ) {
+  exit(data) {
     this.visitorService
       .updateExitTime(
         data.id,
         data.visitSummary.visitNumber,
         data.visitSummary.remarks
       )
-      .subscribe( val => {
+      .pipe(tap(this.rest.createNotifySnackbar("successfully-exited")))
+      .subscribe(val => {
         this.refreshData();
-      } );
+      });
   }
 
-  applyFilter( filterValue: string ) {
+  applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
