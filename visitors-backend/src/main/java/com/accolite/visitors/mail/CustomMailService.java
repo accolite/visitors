5146bb22eb55f8ea99bc2ec784c0457030fd7ee1
Rahul Kumar <rahul.k@accoliteindia.com>
2019-08-01 15:25:11 +0530

    ReadFile bug fixed

--- visitors-backend/src/main/java/com/accolite/visitors/mail/CustomMailService.java
+++ visitors-backend/src/main/java/com/accolite/visitors/mail/CustomMailService.java
@@ -1,11 +1,9 @@
 package com.accolite.visitors.mail;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,7 +23,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
 
 import com.accolite.visitors.db.VisitorsMongoData;
 import com.accolite.visitors.enums.VisitorStatus;
