7d989cb5835baf2ede6ece381549b43938d81e1f
shashikant <shashikant.mittapelli@accoliteindia.com>
2019-08-05 13:04:32 +0530

    changes made

--- visitors-frontend/src/app/components/main/main.component.ts
+++ visitors-frontend/src/app/components/main/main.component.ts
@@ -4,11 +4,11 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { accoliteLocation } from 'src/app/helpers/static-data';
 
 @Component( {
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: [ './dashboard.component.css' ]
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: [ './main.component.css' ]
 } )
-export class DashboardComponent implements OnInit {
+export class MainComponent implements OnInit {
   active: any;
   title = 'visitor-frontend';
   location: any = accoliteLocation;
