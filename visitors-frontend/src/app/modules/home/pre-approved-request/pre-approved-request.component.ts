ecd2572cdf26be10a659dd10e5b51c01c4c84ecd
seema-bhooteshwar <seema.bhooteshwar@accoliteindia.com>
2019-08-06 14:56:24 +0530

    bug fixing

--- visitors-frontend/src/app/modules/home/pre-approved-request/pre-approved-request.component.ts
+++ visitors-frontend/src/app/modules/home/pre-approved-request/pre-approved-request.component.ts
@@ -16,11 +16,11 @@ import { DialogOverviewComponent } from "../dialog-overview/dialog-overview.comp
 
 import { dataModel } from "../dialog-overview/dataModel";
 
-@Component({
+@Component( {
   selector: "app-pre-approved-request",
   templateUrl: "./pre-approved-request.component.html",
-  styleUrls: ["./pre-approved-request.component.css"]
-})
+  styleUrls: [ "./pre-approved-request.component.css" ]
+} )
 export class PreApprovedRequestComponent extends DataObtainer<any> {
   visitors: any;
   pagination = false;
@@ -37,16 +37,16 @@ export class PreApprovedRequestComponent extends DataObtainer<any> {
   @Input()
   pending: PendingRequestComponent;
 
-  @ViewChild(MatPaginator, { static: true })
+  @ViewChild( MatPaginator, { static: true } )
   paginator: MatPaginator;
 
-  @ViewChild(MatSort, { static: true })
+  @ViewChild( MatSort, { static: true } )
   sort: MatSort;
 
   @Input()
   dataSource: MatTableDataSource<any>;
 
-  displayedColumns = ["Name", "badgeNo", "contactPerson", "actions", "remarks"];
+  displayedColumns = [ "Name", "badgeNo", "contactPerson", "actions", "remarks" ];
   ofcLocation: any;
 
   constructor(
@@ -55,22 +55,22 @@ export class PreApprovedRequestComponent extends DataObtainer<any> {
     private rest: RestService,
     public dialog: MatDialog
   ) {
-    super(zone);
+    super( zone );
   }
 
   ngOnInit() {
     this.refreshData();
   }
-  getDataObservable(params: ServiceSearchParamsInputModel) {
+  getDataObservable( params: ServiceSearchParamsInputModel ) {
     this.searchObj = {
       status: "SCHEDULED",
       officeLocation: this.ofcLocation
     };
 
-    return this.visitorService.searchVisitor(this.searchObj);
+    return this.visitorService.searchVisitor( this.searchObj );
   }
 
-  onAfterUpdateData(data: any) {
+  onAfterUpdateData( data: any ) {
     this.visitors = data && data.data ? data.data : null;
     this.dataSource = new MatTableDataSource(
       this.visitors ? this.visitors : []
@@ -78,36 +78,36 @@ export class PreApprovedRequestComponent extends DataObtainer<any> {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
-  cancelled(event) {
+  cancelled( event ) {
     this.visitorSummaryObj = {
-      visitNumber: event["visitSummary"].visitNumber, // Existing one no need to change
-      badgeNo: event["visitSummary"].badgeNo,
-      comingFrom: event["visitSummary"].comingFrom,
-      contactPerson: event["visitSummary"].contactPerson,
-      contactPersonEmailId: event["visitSummary"].contactPersonEmailId,
-      contactPersonPhone: event["visitSummary"].contactPersonPhone,
-      purpose: event["visitSummary"].purpose,
-      officeLocation: event["visitSummary"].officeLocation,
-      inTime: event["visitSummary"].inTime,
-      outTime: event["visitSummary"].outTime,
+      visitNumber: event[ "visitSummary" ].visitNumber, // Existing one no need to change
+      badgeNo: event[ "visitSummary" ].badgeNo,
+      comingFrom: event[ "visitSummary" ].comingFrom,
+      contactPerson: event[ "visitSummary" ].contactPerson,
+      contactPersonEmailId: event[ "visitSummary" ].contactPersonEmailId,
+      contactPersonPhone: event[ "visitSummary" ].contactPersonPhone,
+      purpose: event[ "visitSummary" ].purpose,
+      officeLocation: event[ "visitSummary" ].officeLocation,
+      inTime: event[ "visitSummary" ].inTime,
+      outTime: event[ "visitSummary" ].outTime,
       status: "CANCELLED",
-      scheduledTime: event["visitSummary"].scheduledTime,
-      remarks: event["visitSummary"].remarks
+      scheduledTime: event[ "visitSummary" ].scheduledTime,
+      remarks: event[ "visitSummary" ].remarks
     };
     this.visitorService
-      .updateVisitSummary(event.id, this.visitorSummaryObj)
-      .pipe(tap(this.rest.createNotifySnackbar("successfully-cancelled")))
-      .subscribe(() => {
+      .updateVisitSummary( event.id, this.visitorSummaryObj )
+      .pipe( tap( this.rest.createNotifySnackbar( "successfully-cancelled" ) ) )
+      .subscribe( () => {
         this.refreshData();
-      });
+      } );
   }
-  assignBadge(event) {
-    const dialogRef = this.dialog.open(DialogOverviewComponent, {
+  assignBadge( event ) {
+    const dialogRef = this.dialog.open( DialogOverviewComponent, {
       width: "500px",
       data: event
-    });
+    } );
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe( result => {
       this.visitorData = {
         badgeNo: result.data.visitSummary.badgeNo,
 
@@ -127,7 +127,7 @@ export class PreApprovedRequestComponent extends DataObtainer<any> {
 
       this.visitor = {
         // badgeNo: result.data.visitSummary.badgeNo,
-        visitSummary: [result.data.visitSummary],
+        visitSummary: [ result.data.visitSummary ],
 
         // contactPerson: result.data.visitSummary.contactPerson,
         // comingFrom: result.data.visitSummary.comingFrom,
@@ -151,19 +151,19 @@ export class PreApprovedRequestComponent extends DataObtainer<any> {
         visitorType: result.data.visitorType,
         phoneNumber: result.data.phoneNumber
       };
-      console.log(result.data);
+      console.log( result.data );
       this.visitorService
-        .updateVisitSummary(result.data.id, this.visitorData)
-        .subscribe(() => {
-          this.visitorService.sendNotifyMail(this.visitor).subscribe();
+        .updateVisitSummary( result.data.id, this.visitorData )
+        .subscribe( () => {
+          this.visitorService.sendNotifyMail( this.visitor ).subscribe();
           this.refreshData();
-          if (this.pending) {
+          if ( this.pending ) {
             this.pending.refreshData();
           }
-        });
-    });
+        } );
+    } );
   }
-  applyFilter(filterValue: string) {
+  applyFilter( filterValue: string ) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
