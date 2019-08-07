2192cc48e850cfa7f99a3906876e35f8f9d9158f
Azam <mohammad.azam@accoliteindia.com>
2019-08-07 15:41:02 +0530

    added multi select drop down

--- visitors-frontend/src/app/modules/home/home.component.ts
+++ visitors-frontend/src/app/modules/home/home.component.ts
@@ -5,15 +5,16 @@ import { ApprovedRequestComponent } from "./approved-request/approved-request.co
 import { PendingRequestComponent } from "./pending-request/pending-request.component";
 import { PreApprovedRequestComponent } from "./pre-approved-request/pre-approved-request.component";
 import { ActivatedRoute } from "@angular/router";
+import { ArrayUtil } from 'src/app/helpers/array.util';
 @Component( {
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: [ "./home.component.css" ]
 } )
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   visitors: any;
   pagination = false;
-  ofcLocation: string;
+  ofcLocation: Array<string>;
 
   @ViewChild( MatPaginator, { static: true } )
   paginator: MatPaginator;
@@ -44,22 +45,48 @@ export class HomeComponent implements OnInit {
 
   displayedColumns = [ "badgeNo", "Name", "inTime", "actions", "remarks" ];
 
-  constructor( private route: ActivatedRoute ) { }
+  constructor( private route: ActivatedRoute ) {
+    this.changeParamData()
+  }
 
-  ngOnInit() {
-    this.route.queryParams.subscribe( params => {
-      this.approved.ofcLocation = params.loc;
-      this.approved.refreshData();
-      this.pending.ofcLocation = params.loc;
-      this.pending.refreshData();
-      this.preApproved.ofcLocation = params.loc;
-      this.preApproved.refreshData();
-    } );
+  // ngOnInit() {
+  //   this.route.queryParams.subscribe( params => {
+  //     this.approved.ofcLocation = JSON.parse( JSON.stringify( this.ofcLocation ) );
+  //     this.approved.refreshData();
+  //     this.pending.ofcLocation = JSON.parse( JSON.stringify( this.ofcLocation ) );
+  //     this.pending.refreshData();
+  //     this.preApproved.ofcLocation = JSON.parse( JSON.stringify( this.ofcLocation ) );
+  //     this.preApproved.refreshData();
+  //   } );
+  // }
+
+  ngDoCheck() {
+    let localUrl: any = this.checkUrl() && this.getLocation() ? this.getLocation() : [];
+    if ( ArrayUtil.isNotEmpty( localUrl ) && localUrl.sort().toString() != this.ofcLocation.sort().toString() ) {
+      this.ofcLocation = JSON.parse( JSON.stringify( localUrl ) )
+    }
   }
 
   ngAfterViewInit() {
     this.preapprovedChange.emit( this.preApproved );
     this.approvedChange.emit( this.approved );
     this.pendingChange.emit( this.pending );
+  }
+
+  changeParamData() {
+    if ( this.checkUrl() ) {
+      this.ofcLocation = this.getLocation()
+    } else {
+      this.ofcLocation = [ 'Bangalore' ];
+    }
+  }
+
+  private checkUrl() {
+    return window.location.href.indexOf( '?loc' ) > -1;
+  }
+
+  private getLocation() {
+    let value: string = window.location.href.split( "=" )[ 1 ];
+    return value.split( ',' );
   }
 }
